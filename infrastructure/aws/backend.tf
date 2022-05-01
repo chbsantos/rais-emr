74f160af09a03df56a77f6361f3a25fcb0ec5a0b
chsantos <chsantos@stoneridge.com>
2022-05-01 16:22:44 -0300

    alteracao do nome do bucket de controle de estado do terraform

--- infrastructure/aws/backend.tf
+++ infrastructure/aws/backend.tf
@@ -1,7 +1,7 @@
 # Backend configuration require a AWS storage bucket.
 terraform {
   backend "s3" {
-    bucket = "terraform-state-igti-ney"
+    bucket = "terraform-state-carlos-072163000909"
     key    = "state/igti/edc/mod1/desafio_final_v2/terraform.tfstate"
     region = "us-east-2"
   }
