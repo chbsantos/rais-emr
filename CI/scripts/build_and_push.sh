462fdb7bc725753d9d51845f49b9135c35eda967
chsantos <chsantos@stoneridge.com>
2022-05-01 22:05:01 -0300

    acerto de commits errados

--- CI/scripts/build_and_push.sh
+++ CI/scripts/build_and_push.sh
@@ -4,7 +4,7 @@ set -e
 cd etl/
 
 # Push to AWS ECR
-aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 127012818163.dkr.ecr.sa-east-1.amazonaws.com
-docker build -t igti-ney-prod-extract-rais .
-docker tag igti-ney-prod-extract-rais:latest 127012818163.dkr.ecr.sa-east-1.amazonaws.com/igti-ney-prod-extract-rais:latest
-docker push 127012818163.dkr.ecr.sa-east-1.amazonaws.com/igti-ney-prod-extract-rais:latest
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 072163000909.dkr.ecr.us-east-2.amazonaws.com
+docker build -t carlos-prod-extract-rais .
+docker tag carlos-prod-extract-rais:latest 072163000909.dkr.ecr.us-east-2.amazonaws.com/carlos-prod-extract-rais:latest
+docker push 072163000909.dkr.ecr.us-east-2.amazonaws.com/carlos-prod-extract-rais:latest
